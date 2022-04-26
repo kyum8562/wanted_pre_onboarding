e0e0cfc5fb07c8156ac476fa6de6b5a5bbcbd410
kyum8562@gmail.com <66922359+kyum8562@users.noreply.github.com>
2022-04-26 21:00:27 +0900

    0426

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import '../css/Input.css';
 
 function Input(){
+    // 이메일
     const [email, setEmail] = useState('');
     const [isEmail, setIsEmail] = useState(false);
     const emailRegex =
@@ -9,29 +10,39 @@ function Input(){
     const onChangeEmail = (e) => {
         const emailCurrent = e.target.value;
         setEmail(emailCurrent);
-        if(!emailRegex.test(emailCurrent)){
-            setIsEmail(false);
-        }
-        else{
-            setIsEmail(true);
-        }
+        if(!emailRegex.test(emailCurrent)) setIsEmail(false);
+        else setIsEmail(true);
     }
-    console.log(email + ":" , isEmail);
+
+    // 패스워드
+    const [pwd, setPwd] = useState({type: 'password', visible: false})
+    const onClickBtn = (e) => {
+        if(pwd.type === 'password') setPwd({type: "text", visible:true});
+        else setPwd({type: "password", visible:false});
+    }
+    // console.log(email + ":" , isEmail);
     return(
         <div className='container'>
             <div className='input-box'>
                 <div className='email-box'>
-                    <caption>Email</caption>
+                    {/* <caption>Email</caption> */}
                     <input type='email'
                         placeholder='E-mail'
                         className='email'
                         onChange={onChangeEmail}/>
                 </div>
                 <div className='password-box'>
-                    <caption>Password</caption>
-                    <input type='password'
+                    {/* <caption>Password</caption> */}
+                    <input type={pwd.type}
                         placeholder='Password'
-                        className='password' />
+                        className='password'>
+                    </input>
+                    <div className='img-box'
+                        onClick={onClickBtn}>
+                        {/* {pwd.visible ?
+                        <img id='i1' src={require('../img/show.png')} /> : 
+                        <img id='i2' src={require('../img/hidden.png')}/>}                        */}
+                    </div>
                 </div>
             </div>
         </div>
